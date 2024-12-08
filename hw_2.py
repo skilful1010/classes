da0832641ac77f1c5d6b57f7eff471c0db6ddf77
Neto test <skilful1010@mail.ru>
2024-12-09 00:41:41 +0300

    Initial commit

---
+++ hw_2.py
@@ -0,0 +1,45 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def lecturer_rate(self, lecturer, course, grade):
+        if (isinstance(lecturer, Lecturer) and course in lecturer.courses_attached
+                and course in self.courses_in_progress):
+            if course in lecturer.lecturer_grades:
+                lecturer.lecturer_grades[course] += [grade]
+            else:
+                lecturer.lecturer_grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.lecturer_grades = {}
